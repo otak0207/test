0624766e681eb2d31f55c83f9145bbb999fa3e8e
otak <otak@atrasc.co.jp>
2020-11-15 09:42:58 +0900

    commit Test Create Input Form

---
+++ test/FormController.swift
@@ -0,0 +1,30 @@
+//
+//  FormController.swift
+//  test
+//
+//  Created by Katsu on 2020/11/15.
+//  Copyright © 2020 atrasc. All rights reserved.
+//
+
+import UIKit
+
+class FormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
